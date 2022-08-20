97a108c0f018caf895ee78010f1c3cecc75b5f4f
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-08-20 02:22:47 +0200

    Added non web modules to module tree

--- web_framework_lib/src/lib.rs
+++ web_framework_lib/src/lib.rs
@@ -4,6 +4,15 @@ pub mod web {
     pub mod server;
 }
 
+pub mod application {
+    pub mod di {
+        pub mod container;
+        pub mod providable_trait;
+        pub mod provider_trait;
+    }
+    pub mod application_context;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]

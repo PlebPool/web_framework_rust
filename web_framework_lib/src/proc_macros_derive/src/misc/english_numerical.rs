42f531018c821d2360cfdcd90514a5103b9a6c9b
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-09-01 01:46:28 +0200

    change 'lingua_franca' to 'english'

--- web_framework_lib/src/proc_macros_derive/src/misc/english_numerical.rs
+++ web_framework_lib/src/proc_macros_derive/src/misc/english_numerical.rs
@@ -1,4 +1,4 @@
-pub fn match_numerica_to_lingua_franca<'a>(c: char) -> &'a str {
+pub fn match_numeric_to_english<'a>(c: char) -> &'a str {
     match c {
         '0' => { "ZERO" }, '1' => { "ONE" }, '2' => { "TWO" }, '3' => { "THREE" }, '4' => { "FOUR" },
         '5' => { "FIVE" }, '6' => { "SIX" }, '7' => { "SEVEN" }, '8' => { "EIGHT" }, '9' => { "NINE" },
@@ -6,7 +6,7 @@ pub fn match_numerica_to_lingua_franca<'a>(c: char) -> &'a str {
     }
 }
 
-pub fn match_lingua_franca_to_numerical(s: &str) -> char {
+pub fn match_english_to_numeric(s: &str) -> char {
     match s.to_uppercase().as_str() {
         "ZERO" => { '0' }, "ONE" => { '1' }, "TWO" => { '2' }, "THREE" => { '3' }, "FOUR" => { '4' },
         "FIVE" => { '5' }, "SIX" => { '6' }, "SEVEN" => { '7' }, "EIGHT" => { '8' }, "NINE" => { '9' },
@@ -14,7 +14,7 @@ pub fn match_lingua_franca_to_numerical(s: &str) -> char {
     }
 }
 
-pub fn starts_with_numerical_lingua_franca(s: &str) -> Result<&str, ()> {
+pub fn starts_with_numeric_english(s: &str) -> Result<&str, ()> {
     const VEC: &[&str] = &["ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN",
         "EIGHT", "NINE"];
     for a in VEC {

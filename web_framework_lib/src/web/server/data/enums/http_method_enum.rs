e160a3d4f51c9a86b17c3dd6447207704ca4aeec
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-09-01 22:24:17 +0200

    Removed boilerplate and used proc macro instead

--- web_framework_lib/src/web/server/data/enums/http_method_enum.rs
+++ web_framework_lib/src/web/server/data/enums/http_method_enum.rs
@@ -1,33 +1,10 @@
-use std::str::FromStr;
+use proc_macros_derive::EnumFromStr;
+use proc_macros_derive::StrFromEnum;
 
+#[derive(EnumFromStr, StrFromEnum)]
 pub enum HttpMethod {
     GET,
     POST,
     PUT,
     DELETE
-}
-
-impl FromStr for HttpMethod {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_uppercase().as_str() {
-            "POST" => { Ok(HttpMethod::POST) },
-            "GET" => { Ok(HttpMethod::GET) },
-            "PUT" => { Ok(HttpMethod::PUT) },
-            "DELETE" => { Ok(HttpMethod::DELETE) },
-            _ => { Err(()) }
-        }
-    }
-}
-
-impl ToString for HttpMethod {
-    fn to_string(&self) -> String {
-        match self {
-            HttpMethod::GET => { "GET".to_string() },
-            HttpMethod::POST => { "POST".to_string() },
-            HttpMethod::PUT => { "PUT".to_string() },
-            HttpMethod::DELETE => { "DELETE".to_string() }
-        }
-    }
 }

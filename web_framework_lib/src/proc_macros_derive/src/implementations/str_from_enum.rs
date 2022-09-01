c80729b2e86f605fb4641471bbe7c3c3d81b5cbb
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-09-01 22:26:51 +0200

    added mimicking of relevant key value pair in str_from_enum when csv file not specified

--- web_framework_lib/src/proc_macros_derive/src/implementations/str_from_enum.rs
+++ web_framework_lib/src/proc_macros_derive/src/implementations/str_from_enum.rs
@@ -2,16 +2,16 @@ use proc_macro::TokenStream;
 use quote::quote;
 use std::str::Split;
 use std::fs;
+use std::ops::Add;
 use syn::{Attribute, Variant};
-use crate::english_numerical::match_numeric_to_english;
+
 
 struct StrFromEnumAttrs {
     optional_csv_file_path: Option<String>,
 }
 
 impl StrFromEnumAttrs {
-    pub fn new(input: Vec<Attribute>) -> Self {
-        let attrs: Vec<Attribute> = input;
+    pub fn new(attrs: Vec<Attribute>) -> Self {
         let mut a: Vec<&Attribute> = attrs.iter()
             .filter(|attr: &&Attribute| attr.path.get_ident().unwrap().to_string() == "optional_csv_file_path")
             .collect();
@@ -30,12 +30,12 @@ pub fn impl_mime_type(derive_input: syn::DeriveInput) -> TokenStream {
 
     let variants_as_str: Vec<String> = if let syn::Data::Enum(data_enum) = data {
         data_enum.variants.iter().map(|v: &Variant| {
-            let mut tmp: String = v.ident.to_string();
-            let first_char: char = tmp.chars().nth(0).unwrap();
-            if first_char.is_numeric() {
-                tmp = tmp.replace(first_char,
-                                  match_numeric_to_english(first_char));
-            }
+            let tmp: String = v.ident.to_string();
+            // let first_char: char = tmp.chars().nth(0).unwrap();
+            // if first_char.is_numeric() {
+            //     tmp = tmp.replace(first_char,
+            //                       match_numeric_to_english(first_char));
+            // }
             tmp
         }).collect()
     } else {
@@ -44,51 +44,57 @@ pub fn impl_mime_type(derive_input: syn::DeriveInput) -> TokenStream {
 
     let attr_struct: StrFromEnumAttrs = StrFromEnumAttrs::new(attrs.clone());
 
-    let csv_file_path: &String;
-    match attr_struct.optional_csv_file_path() {
-        Some(p) => { csv_file_path = p; dbg!(&csv_file_path); },
-        None => {
-            panic!("You need to specify helper attribute #[optional_csv_file_path(path)], I love irony.")
+    let data: String = match attr_struct.optional_csv_file_path() {
+        Some(p) => {
+            let csv_file_path: &String = p;
+            dbg!(&csv_file_path);
+             String::from_utf8_lossy(&fs::read(csv_file_path.as_str())
+                 .expect("Failed to read CSV file.")).to_string()
+        },
+        None => { // We'll mimic a csv file in a string here.
+            let mut result: String = String::new();
+            for variant in variants_as_str.as_slice() {
+                result = result.add(
+                    &format!("{};{}\n", variant, variant)
+                );
+            }
+            result
         }
-    }
-    let file_as_u8_vec: Vec<u8> = fs::read(csv_file_path.as_str())
-        .expect("Failed to read CSV file.");
-    let file_as_str: &str = &*String::from_utf8_lossy(&file_as_u8_vec);
-    let file_as_str_newline_split: Split<char> = file_as_str.split('\n');
+    };
+    dbg!(&data);
+    let data_as_str_newline_split: Split<char> = data.split('\n');
     let mut keys: Vec<proc_macro2::TokenStream> = Vec::new();
     let mut vals: Vec<String> = Vec::new();
-    for line in file_as_str_newline_split {
-        println!("MimeType: {}", line);
-        let line_separator_split: (&str, &str) = line.split_once(';').unwrap();
-        let mut key: String = line_separator_split.0.replace('.', "").to_uppercase();
-        // Checking if first char of key is numeric.
-        let first_char: char = key.chars().nth(0).unwrap();
-        if first_char.is_numeric() {
-            // Replacing first char with english word for numeric value.
-            let num_in_lingua_franca: &str = crate::english_numerical::match_numeric_to_english(key.chars().nth(0)
-                .expect("No first char WTF"));
-            key = key.replacen(first_char, num_in_lingua_franca, 1);
-        }
-        // If key from csv file is represented in enum.
-        if variants_as_str.contains(&key) {
-            keys.push(key.parse().unwrap());
-            vals.push(line_separator_split.1.replace("\r\n", "")
-                .replace("\r", ""));
-        }
+    for line in data_as_str_newline_split {
+        let line_separator_split: Option<(&str, &str)> = line.split_once(";");
+        let _ = line_separator_split.map(|(key, val): (&str, &str)| {
+            let mut key: String = String::from(key);
+            let k_first_char: char = key.chars().nth(0).expect("Key lacks first char");
+            if k_first_char.is_numeric() {
+                let num_in_english: &str = crate::english_numerical::match_numeric_to_english(k_first_char);
+                key = key.replacen(k_first_char, num_in_english, 1);
+            }
+            key = key.replace(".", "").to_uppercase();
+            if variants_as_str.contains(&key) {
+                keys.push(key.parse().unwrap());
+                vals.push(val.replace("\r\n", "")
+                    .replace("\r", ""));
+            }
+        });
     }
     let quote: proc_macro2::TokenStream = quote! {
-        impl #ident {
-            pub fn as_str(&self) -> Result<String, ()> {
+        impl ToString for #ident {
+            fn to_string(&self) -> String {
                 match self {
                     #(#ident::#keys => {
-                        Ok( String::from( #vals ) )
+                        String::from( #vals )
                     },)*_ => {
-                        Err(())
+                        panic!("to_string failed.")
                     }
                 }
             }
         }
     };
-    println!("Generated match stmt:\r\n{}", quote);
+    println!("Generated match stmt:\r\n{}", &quote.to_string());
     TokenStream::from(quote)
 }

d463a08d406913c7fd1f7ad8c865b308e0587598
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-09-26 08:53:54 +0200

    we do a bit of trolling

--- web_framework_lib/src/web/util/parsers/json_parser.rs
+++ web_framework_lib/src/web/util/parsers/json_parser.rs
@@ -5,12 +5,18 @@ enum ProcessingModes {
     KeyValDelimiter,
     ArrayStart,
     ArrayEnd,
-    KeyOrValStart,
+    KeyOrValStartOrEnd,
     KeyValSwitch,
     EmptySpace,
     NonReservedCharacter
 }
 
+// Binary    Hex          Comments
+// 0xxxxxxx  0x00..0x7F   Only byte of a 1-byte character encoding
+// 10xxxxxx  0x80..0xBF   Continuation byte: one of 1-3 bytes following the first
+// 110xxxxx  0xC0..0xDF   First byte of a 2-byte character encoding
+// 1110xxxx  0xE0..0xEF   First byte of a 3-byte character encoding
+// 11110xxx  0xF0..0xF7   First byte of a 4-byte character encoding
 pub fn parse_into_json_objects(s: &str) {
     dbg!(s);
     let trimmed: &str = s.trim();
@@ -23,13 +29,13 @@ pub fn parse_into_json_objects(s: &str) {
             91 => { depth_index = depth_index + 1; ProcessingModes::ArrayStart  },
             93 => { depth_index = depth_index - 1; ProcessingModes::ArrayEnd },
             44 => { ProcessingModes::KeyValDelimiter },
-            34 => { ProcessingModes::KeyOrValStart },
+            34 => { ProcessingModes::KeyOrValStartOrEnd },
             58 => { ProcessingModes::KeyValSwitch }
             32 => { ProcessingModes::EmptySpace }
             _ => { ProcessingModes::NonReservedCharacter }
         };
-        if b != 32 {
-            println!("{}: {:#?}: {}", char::from(b), switch, depth_index);
+        if b != 32 && b != 10 {
+            println!("char: {}, switch: {:#?}, depth_index: {}", char::from(b), switch, depth_index);
         }
     }
 }

cad7b3c67393205e224adf61143574992ea4e029
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-09-29 01:03:12 +0200

    I feel like this is way to much code for what it's doing

--- web_framework_lib/src/web/util/parsers/json_parser.rs
+++ web_framework_lib/src/web/util/parsers/json_parser.rs
@@ -19,10 +19,28 @@ enum JsonVariant {
 
 impl JsonObject {
     pub fn parse(arr: &[u8]) -> Self {
-        let mut in_val: bool = false;
+        let mut vec: Vec<u8> = Self::remove_spaces_surgically(arr);
+        vec = Self::slice_trailing_and_prefixing_curly_brackets(vec);
+        let vec: Vec<&[u8]> = Self::split_top_level_properties_by_commas(&vec);
+        let (key_vec, val_vec):
+            (Vec<(&[u8], usize)>, Vec<(JsonVariant, Vec<u8>)>) = Self::extract_keys_and_values(vec);
 
+        if log::log_enabled!(log::Level::Debug) {
+            println!("key_vec_len = {}, val_vec_len = {}", key_vec.len(), val_vec.len());
+        }
+        if key_vec.len() != val_vec.len() {
+            // TODO
+            panic!("Bad format.")
+        }
+        Self {
+            name: "hey".to_string()
+        }
+    }
+
+    fn remove_spaces_surgically(arr: &[u8]) -> Vec<u8> {
+        let mut in_val: bool = false;
         // We trim the array, removing trailing and prefixing spaces.
-        let arr: Vec<u8> = trim_byte_array(arr)
+        let arr: Vec<u8> = Self::trim_byte_array(arr)
             .into_iter() // Turn it into an iterator.
             .map(|b: &u8| *b) // We deref every element once.
             .filter(|byte: &u8| { // we remove spaces outside of keys and vals. Also newlines.
@@ -30,130 +48,113 @@ impl JsonObject {
                 !(!in_val && (*byte == 32 || *byte == 10))
             })
             .collect::<Vec<u8>>(); // We collect result in an array.
-        // Because we need it in a variable for next step.
+        arr
+    }
 
+    fn trim_byte_array(arr: &[u8]) -> &[u8] {
+        if arr.len() > 0 {
+            let (mut i, mut j): (usize, usize) = (0, arr.len());
+            loop {
+                if arr[i] != 32 && arr[j - 1] != 32 { break; }
+                else {
+                    if arr[i] == 32 { i = i + 1; }
+                    if arr[j - 1] == 32 { j = j - 1; }
+                }
+            }
+            &arr[i..j]
+        } else { arr }
+    }
+
+    fn slice_trailing_and_prefixing_curly_brackets(vec: Vec<u8>) -> Vec<u8> {
         // Making sure the first character is "{" (123), and the last is "}" (125).
-        if !(arr[0] == 123 && arr[arr.len()-1] == 125) {
+        if !(vec[0] == 123 && vec[vec.len()-1] == 125) {
+            // TODO
             panic!("INVALID FIRST AND LAST BYTE");
         }
         // Slicing contents of outer curly brackets.
-        let arr: &[u8] = &arr[1..arr.len()-1];
+        vec[1..vec.len()-1].to_vec()
+    }
 
+    fn split_top_level_properties_by_commas<'a>(vec: &'a Vec<u8>) -> Vec<&'a [u8]> {
         let mut in_big_val: bool = false; // If we're in an array or object.
-        let arr: Vec<&[u8]> = arr.split(|byte: &u8| {
+        vec.split(|byte: &u8| {
             // We split by commas "," if the comma is not inside of a value.
-            if *byte == 91 || *byte == 123 {
-                in_big_val = true;
-            } else if *byte == 93 || *byte == 125 {
-                in_big_val = false;
-            }
+            if *byte == 91 || *byte == 123 { in_big_val = true; }
+            else if *byte == 93 || *byte == 125 { in_big_val = false; }
             !in_big_val && *byte == 44
         })
-            .collect::<Vec<&[u8]>>();
-        // We collect result in array because we need it in a variable for the next step.
+            .collect::<Vec<&[u8]>>()
+    }
 
-        // This vector holds all the keys, it also holds the index to the value. Which is in a seperate vector.
-        let mut key_vec: Vec<(&[u8], usize)> = Vec::new();
-        // This vector hold all the values, it also holds the JsonVariant of the value.
-        let mut val_vec: Vec<(JsonVariant, Vec<u8>)> = Vec::new();
-        // Temporary buffer used for multiline values like arrays and objects that need to concatonated.
-        let mut val_buffer: Vec<u8> = Vec::new();
-        // The current index, this is needed as we're synchronizing the indexes of the key and val vectors.
-        let mut current: usize = 0;
-        // Needed for EOF flushing of the buffer.
-        let len: usize = arr.iter().count();
-
-        let _ = arr
+    fn extract_keys_and_values<'a>(vec: Vec<&'a [u8]>) -> (Vec<(&'a [u8], usize)>, Vec<(JsonVariant, Vec<u8>)>) {
+        let (mut key_vec, mut val_vec):
+            (Vec<(&'a [u8], usize)>, Vec<(JsonVariant, Vec<u8>)>) = (Vec::new(), Vec::new());
+        let len: usize = vec.iter().count();
+        let _ = vec
             .into_iter()
-            .map(|byte_slice: &[u8]| {
-                // This map returns a tuple containing either (key, val) or (None, val).
-                // Depending on if the line we're processing holds a key or not.
-                // If it returns (None, val) we concat the value onto the previous one.
-                // This is why we have the "val_buffer" above.
-                if byte_slice[0] == 123 || byte_slice[0] == 91 {
-                    // If it starts with either 123 or 91, then the line is a a part of a
-                    // multiproperty value like an array or and object.
-                    return (None, Some(byte_slice))
-                }
-                // Getting the index of possible key val delimiter.
-                let index: Option<usize> = byte_slice.iter().position(|byte: &u8| *byte == 58);
-                // If we have an index, we have a key and a value.
-                // If we don't, then we only have a value.
-                match index {
-                    Some(index) => {
-                        let colon_slice: (&[u8], &[u8]) = byte_slice.split_at(index);
-                        // The slice in the second element is to get rid of the colon,
-                        // this is needed because split_at() is inclusive.
-                        (Some(colon_slice.0), Some(&colon_slice.1[1..colon_slice.1.len()]))
-                    },
-                    None => {
-                        // The slice in the second element is to get rid of the colon,
-                        // this is needed because split_at() is inclusive.
-                        (None, Some(&byte_slice[1..byte_slice.len()]))
-                    }
-                }
-            })
-            .enumerate() // For EOF flushing.
-            .map(|(i, (key, val)): (usize, (Option<&[u8]>, Option<&[u8]>))| {
-                let mut flush: bool = false; // Whether we flush buffer or not.
-                key.map(|k: &[u8]| {
-                    flush = !val_buffer.is_empty();
-                    // Current index, we're not decrementing with 1, because the val_vec will grow,
-                    // later on in this map instance.
-                    current = val_vec.len();
-                    // Pushing key and index of future value attributed to it.
-                    key_vec.push((k, current));
-                    // Clearing buffer.
-                    val_buffer.clear();
-                });
-                if log::log_enabled!(log::Level::Debug) {
-                    key.map(|b| {
-                        println!("{}: Json key: {}", current, String::from_utf8_lossy(b));
-                    });
-                    val.map(|b: &[u8]| {
-                        println!("{}: Json val: {}", current, String::from_utf8_lossy(b));
-                    });
-                }
-                val.map(|v: &[u8]| {
-                    // We're not flushing if the value starts with 123 or 91.
-                    flush = !(v[0] == 123 || v[0] == 91);
-                    val_buffer.append(&mut v.to_vec());
-                });
-                // We flush the buffer if flush is true or if we're on EOF.
-                if flush || i == len - 1 {
-                    if !val_buffer.is_empty() {
-                        let variant: JsonVariant =
-                            match val_buffer[1] {
-                                123 => { JsonVariant::JsonObject },
-                                91 => { JsonVariant::JsonArray },
-                                _ => { JsonVariant::JsonValue }
-                            };
-                        val_vec.push((variant, val_buffer.to_owned()));
-                    }
-                }
-            }).count(); // Triggering map.
-        if log::log_enabled!(log::Level::Debug) {
-            println!("key_vec_len = {}, val_vec_len = {}", key_vec.len(), val_vec.len());
+            .map(|byte_slice: &[u8]| Self::split_line_into_keys_and_values(byte_slice)).enumerate()
+            .map(|(i, (key, val)): (usize, (Option<&[u8]>, Option<&[u8]>))|
+                Self::parse_line_key_value_split(key, val, &mut key_vec, &mut val_vec, len, i))
+            .count();
+        (key_vec, val_vec)
+    }
+
+    fn split_line_into_keys_and_values<'a>(byte_slice: &'a [u8]) -> (Option<&'a [u8]>, Option<&'a [u8]>) {
+        if byte_slice[0] == 123 || byte_slice[0] == 91 {
+            return (None, Some(byte_slice))
         }
-        Self {
-            name: "hey".to_string()
+        let index: Option<usize> = byte_slice.iter().position(|byte: &u8| *byte == 58);
+        match index {
+            Some(index) => {
+                let colon_slice: (&[u8], &[u8]) = byte_slice.split_at(index);
+                (Some(colon_slice.0), Some(&colon_slice.1[1..colon_slice.1.len()]))
+            },
+            None => { (None, Some(&byte_slice[1..byte_slice.len()])) }
+        }
+    }
+
+    fn parse_line_key_value_split<'a>(key: Option<&'a [u8]>,
+                                      val: Option<&[u8]>,
+                                      key_vec: &mut Vec<(&'a [u8], usize)>,
+                                      val_vec: &mut Vec<(JsonVariant, Vec<u8>)>,
+                                      len: usize, i: usize) {
+        let mut current: usize = 0;
+        let mut val_buffer: Vec<u8> = Vec::new();
+        let mut flush: bool = false;
+        key.map(|k: &[u8]| {
+            flush = !val_buffer.is_empty();
+            current = val_vec.len();
+            key_vec.push((k, current));
+            val_buffer.clear();
+        });
+        // TODO: Put in function.
+        if log::log_enabled!(log::Level::Debug) {
+            key.map(|b: &[u8]| {
+                println!("{}: Json key: {}", current, String::from_utf8_lossy(b));
+            });
+            val.map(|b: &[u8]| {
+                println!("{}: Json val: {}", current, String::from_utf8_lossy(b));
+            });
+        }
+        val.map(|v: &[u8]| {
+            flush = !(v[0] == 123 || v[0] == 91);
+            val_buffer.append(&mut v.to_vec());
+        });
+        if flush || i == len - 1 {
+            if !val_buffer.is_empty() {
+                let variant: JsonVariant =
+                    match val_buffer[1] {
+                        123 => { JsonVariant::JsonObject },
+                        91 => { JsonVariant::JsonArray },
+                        _ => { JsonVariant::JsonValue }
+                    };
+                val_vec.push((variant, val_buffer.to_owned()));
+            }
         }
     }
 }
 
-fn trim_byte_array(arr: &[u8]) -> &[u8] {
-    if arr.len() > 0 {
-        let (mut i, mut j): (usize, usize) = (0, arr.len());
-        loop {
-            if arr[i] != 32 && arr[j - 1] != 32 { break; }
-            else {
-                if arr[i] == 32 { i = i + 1; }
-                if arr[j - 1] == 32 { j = j - 1; }
-            }
-        }
-        &arr[i..j]
-    } else { arr }
-}
+
 
 pub fn parse_into_json_objects(s: &str) {
     let _ = JsonObject::parse(s.as_bytes());
